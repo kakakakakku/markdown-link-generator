35303f605f529a8742f3b9fda93c3d1ffc5c2db2
kakakakakku <y.yoshida22@gmail.com>
2014-11-02 03:20:18 +0900

    右クリックメニューでリンクを作成できるようにした

---
+++ src/background.js
@@ -0,0 +1,11 @@
+chrome.contextMenus.create({
+  title: "Qiita:Team 用のリンクを作成する",
+  onclick: function(info, tab) {
+    var textarea = document.createElement("textarea");
+    document.body.appendChild(textarea);
+    textarea.value = "[" + tab.title + "](" + tab.url + ")"; 
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  }
+});
