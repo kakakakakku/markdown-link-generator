bc5fd6fec1c730d0b5f6f006f9ed45071fba6845
kakakakakku <y.yoshida22@gmail.com>
2017-06-20 19:15:47 +0900

    Added markdown-link-generator

--- src/background.js
+++ src/background.js
@@ -1,9 +1,9 @@
 chrome.contextMenus.create({
-  title: "Qiita:Team 用のリンクを作成する",
+  title: "Markdown 用のリンクを作成する",
   onclick: function(info, tab) {
     var textarea = document.createElement("textarea");
     document.body.appendChild(textarea);
-    textarea.value = "[" + tab.title + "](" + tab.url + ")"; 
+    textarea.value = "[" + tab.title + "](" + tab.url + ")";
     textarea.select();
     document.execCommand("copy");
     document.body.removeChild(textarea);
